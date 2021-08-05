3c1d35ff44fbc535995ac8a53bdb8ac987a922cc
Samanta Ceja <sammmceja@gmail.com>
2021-08-04 21:54:16 -0700

    etch-a-sketch-project in progress

--- src/app.js
+++ src/app.js
@@ -1,88 +1,71 @@
-const grid = document.querySelector('.grid')
-const blockWidth = 100
-const blockHeight = 20
-const boardWidth = 560
+/*const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
 
-const userStart = [230, 10]
-let currentPosition = userStart
+let result = 0
 
-//create block 
-class Block {
-    constructor(xAxis, yAxis) {
-        this.bottomLeft = [xAxis,yAxis]
-        this.bottomRight = [xAxis + blockWidth,yAxis]
-        this.topLeft = [xAxis, yAxis + blockHeight]
-        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
-    }
+
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    } )
+
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
 }
 
-const blocks = [
-    new Block(10,270),
-    new Block(120,270),
-    new Block(230,270),
-    new Block(340,270),
-    new Block(450,270),
-    new Block(10,240),
-    new Block(120,240),
-    new Block(230,240),
-    new Block(340,240),
-    new Block(450,240),
-    new Block(10,210),
-    new Block(120,210),
-    new Block(230,210),
-    new Block(340,210),
-    new Block(450,210),
+function moveMole() {
+    let timerId = null 
+    timerId = setInterval(randomSquare, 500)
+}
+
+randomSquare()*/
+
+const container = document.querySelector('.gridContainer');
+const eachSquare = document.querySelectorAll('div')
+const btn = document.createElement("button");
+
+//Button to change the sie of grid
+btn.innerHTML = "Change";
+btn.type = "submit";
+btn.onclick = function () {
     
+}
+
+makeGrid(16);
+function makeGrid(gridNum) {
+    let square = gridNum * gridNum;
+    container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`
+    for(let i = 0; i <= square; i++){
+        const cell = document.createElement('div');
+        cell.id = 
+        cell.style.border = '1px solid rgb(199, 196, 199)'
+        container.append(cell)
+        
+
+        
+    }
+    const pixels = container.querySelectorAll('div');
+    pixels.forEach(pixels => pixels.addEventListener('mouseover', mouseOver));
     
-]
+}
 
 
-//draw my block 
-function addBlocks () {
-    for (let i = 0; i < blocks.length; i++) {
-        const block = document.createElement('div')
-        block.classList.add('block')
-        block.style.left = blocks[i].bottomLeft[0] + 'px'
-        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-        grid.appendChild(block)
+function mouseOver(){
+    document.querySelector('div').style.backgroundColor = "purple";
+}
 
+//Random color function looping through a series of numbers and letters to create a random #hex code
+function randomColor() {
+    const letters = '0123456789ABCDEF';
+    const color = '#';
+    for(let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-}
-
-addBlocks()
-
-
-//add user
-const user = document.createElement('div')
-user.classList.add('user')
-drawUser()
-grid.appendChild(user)
-
-//draw user
-function drawUser() {
-    user.style.left = currentPosition[0] + 'px'
-    user.style.bottom = [1] + 'px'
-}
-
-//move user
-function moveUser(e) {
-    switch(e.key){
-        case 'ArrowLeft': 
-            if(currentPosition[0] > 0) {
-            currentPosition[0] -= 10
-            drawUser()
-            }
-            break;
-        case 'ArrowRight':
-            if (currentPosition[0] < boardWidth - blockWidth) {
-            currentPosition[0] += 10
-            drawUser()
-            }
-            break;
-
-
-    }
-}
-
-document.addEventListener('keydown', moveUser) 
-
+    document.getElementsByClass
+} 
